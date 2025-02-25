f80b911f09bed50d924a11236197317cd9ee76cb
hannnmstfa <burhanmusthofa1@gmail.com>
2025-02-25 08:42:24 +0700

    first commit

---
+++ app/src/main/java/com/example/utskontak/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.example.utskontak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.aplikasikontak.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
